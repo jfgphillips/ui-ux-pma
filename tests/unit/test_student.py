d7cb3e77840824ccbe4c54275820cad1d8542032
jfgphillips <jfgphillips@gmail.com>
2023-06-19 20:40:54 +0100

    reorganise tests

--- tests/unit/test_student.py
+++ tests/unit/test_student.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy.exc import IntegrityError
 
 from db import db
 from models import StudentModel
@@ -22,6 +23,44 @@ def stub_student_data(app):
         db.session.add(student)
         db.session.commit()
     return student_data
+
+def test_student_delete_error(stub_student_data, client, app):
+    """
+    This test aims to assure that the logic used to ensure the caller is permissioned to delete an account functions
+    as expected. The flow runs as follows
+    1. with a populated db containing a student make a delete request to the students endpoint using a different student id
+    2. assert we get a bad response (401)
+    3. sanity check that we still have the student in the database
+    """
+    expected_student_data = stub_student_data.copy()
+    expected_student_data.pop("password")
+    expected_student_data["registers"] = []
+
+    student_id = stub_student_data["id"]
+    different_student_id = 2
+    assert different_student_id != student_id
+    bad_response = client.delete(
+        f"/students/{student_id}", headers=get_student_authed_header(student_id=different_student_id, app=app)
+    )
+    assert bad_response.status_code == 401
+    students_response = client.get(f"/students/{student_id}")
+    assert students_response.status_code == 200
+    assert students_response.json == expected_student_data
+
+
+def test_student_delete_success(stub_student_data, client, app):
+    """
+    This test aims to check that the student containing the correct credentials can delete their account.
+    We can sanity check this operation by requesting the deleted students data and getting a 404 error indicating there
+    is no entity.
+    """
+    student_id = stub_student_data["id"]
+    good_response = client.delete(
+        f"/students/{student_id}", headers=get_student_authed_header(student_id=student_id, app=app)
+    )
+    assert good_response.status_code == 200
+    students_response = client.get(f"/students/{student_id}")
+    assert students_response.status_code == 404
 
 
 @pytest.mark.parametrize(
@@ -97,44 +136,6 @@ def test_student_create_errors_inconsistencies_bug(bad_data, status_code, stub_s
     assert len(students_response) == 1
 
 
-def test_student_delete_error(stub_student_data, client, app):
-    """
-    This test aims to assure that the logic used to ensure the caller is permissioned to delete an account functions
-    as expected. The flow runs as follows
-    1. with a populated db containing a student make a delete request to the students endpoint using a different student id
-    2. assert we get a bad response (401)
-    3. sanity check that we still have the student in the database
-    """
-    expected_student_data = stub_student_data.copy()
-    expected_student_data.pop("password")
-    expected_student_data["registers"] = []
-
-    student_id = stub_student_data["id"]
-    different_student_id = 2
-    assert different_student_id != student_id
-    bad_response = client.delete(
-        f"/students/{student_id}", headers=get_student_authed_header(student_id=different_student_id, app=app)
-    )
-    assert bad_response.status_code == 401
-    students_response = client.get(f"/students/{student_id}")
-    assert students_response.status_code == 200
-    assert students_response.json == expected_student_data
-
-
-def test_student_delete_success(stub_student_data, client, app):
-    """
-    This test aims to check that the student containing the correct credentials can delete their account.
-    We can sanity check this operation by requesting the deleted students data and getting a 404 error indicating there
-    is no entity.
-    """
-    student_id = stub_student_data["id"]
-    good_response = client.delete(
-        f"/students/{student_id}", headers=get_student_authed_header(student_id=student_id, app=app)
-    )
-    assert good_response.status_code == 200
-    students_response = client.get(f"/students/{student_id}")
-    assert students_response.status_code == 404
-
 
 def test_student_update_constraint_error_bug(stub_student_data, client):
     """
@@ -151,7 +152,6 @@ def test_student_update_constraint_error_bug(stub_student_data, client):
     A better error messages should be used when passing incomplete student data as opposed to the existing integrity error
     that is raised upon db.session.commit() in order not to break the python script. This should allow for a graceful
     blow up.
-
     """
     bad_data = {
         "name": "john Phillips",
@@ -159,8 +159,7 @@ def test_student_update_constraint_error_bug(stub_student_data, client):
         "email": "jfgp111@gmail.com",
         "summary": "Looking for a tutor",
     }
-    # assert stub_student_data["username"] == bad_data["username"]
-    with pytest.raises(Exception):
+    with pytest.raises(IntegrityError):
         client.put("/students/2", json=bad_data)
 
     students_response = client.get("/students").json
